d80c8a901cc3521bf6d8ed2d328faec19a163fe5
Gayatri1076 <gayatriipatil2009@gmail.com>
2026-06-13 12:18:45 +0530

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class abc{
+public String geek;
+public int i;
+public Integer I;
+public abc()
+{
+this.geek="gayu patil";
+}
+public static void main(String[]args)
+{
+abc name=new abc();
+System.out.println("geek name is:"+name.geek);
+System.out.println("Default value for int is"+name.i);
+System.out.println("Default value for int is"+name.i);
+}
+}
