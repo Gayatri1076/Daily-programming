1c3cf52dc646dc6a2b039a9189caffe5621943bb
Gayatri1076 <gayatriipatil2009@gmail.com>
2026-06-17 18:42:23 +0530

    Create ifstatement.java

---
+++ ifstatement.java
@@ -0,0 +1,9 @@
+class ifstatement{
+public static void main(String[]args){
+int number=10;
+//check if number is less than 0
+if(mumber<0){
+System.out.println("The number is negative.");
+}
+System.out.println("Statement outside if block.");
+}
