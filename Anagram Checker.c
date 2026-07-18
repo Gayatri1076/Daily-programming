caba062e6f72e0d44397c753bbd187184c566bf5
Gayatri1076 <gayatriipatil2009@gmail.com>
2026-07-18 20:58:41 +0530

    Create Anagram Checker.c

---
+++ Anagram Checker.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq1[26] = {0}, freq2[26] = {0};
+
+    printf("Enter First String: ");
+    scanf("%s", str1);
+
+    printf("Enter Second String: ");
+    scanf("%s", str2);
+
+    if(strlen(str1) != strlen(str2)) {
+        printf("Not Anagram");
+        return 0;
+    }
+
+    for(int i = 0; str1[i] != '\0'; i++) {
+        freq1[str1[i] - 'a']++;
+        freq2[str2[i] - 'a']++;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        if(freq1[i] != freq2[i]) {
+            printf("Not Anagram");
+            return 0;
+        }
+    }
+
+    printf("Anagram");
+
+    return 0;
+}
