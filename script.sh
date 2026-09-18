d28126819171947a83fa49855d90bbf3d9450baa
Gayatri1076 <gayatriipatil2009@gmail.com>
2026-09-18 15:31:07 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+echo "Hello, $USER!"
+echo "Today's date is: $(date)"
+echo "Your current working directory is: $(pwd)"
+
+# A simple loop example
+echo "Counting to 3:"
+for i in {1..3}
+do
+    echo "Number: $i"
+done
