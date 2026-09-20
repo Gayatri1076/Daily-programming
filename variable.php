8f5813924c30f6d00ded6b4298e86fb978af1cfb
Gayatri1076 <gayatriipatil2009@gmail.com>
2026-09-20 21:48:26 +0530

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Coding in PHP"; // String
+$version = 8.3;          // Float/Double
+$year = 2026;            // Integer
+$is_fun = true;          // Boolean
+
+// Concatenating strings using the dot (.) operator
+echo "Topic: " . $title . " (Year: " . $year . ")";
+?>
+s
